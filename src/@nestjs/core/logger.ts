a99efc2b5aa4a6930a4acd284c42fe11e7c2f2d1
Dadajia-byte <1814650097@qq.com>
2025-03-02 02:42:48 +0800

    feat: 路由解析器

--- src/@nestjs/core/logger.ts
+++ src/@nestjs/core/logger.ts
@@ -1,12 +1,17 @@
 import clc from 'cli-color'; // 可以在控制台打印颜色 
 export class Logger {
+  private static lastTimestamp = Date.now();
   // 定义了一个用来打印日志的工具方法
   static log(message: string, context: string='') {
     // 获取当前时间戳
     const timestamp = new Date().toLocaleString();
     // 获取当前pid
     const pid = process.pid;
-    console.log(`[${clc.green('Nest')}] ${clc.green(pid.toString())}  - ${clc.yellow(timestamp)}}  ${clc.green('LOG ')} [${clc.yellow(context)}]  ${clc.green(message)}`);
+    const currentTimestamp = Date.now();
+    // 计算时间差
+    const diff = currentTimestamp - this.lastTimestamp;
+    console.log(`[${clc.green('Nest')}] ${clc.green(pid.toString())}  - ${clc.yellow(timestamp)}  ${clc.green('LOG ')} [${clc.yellow(context)}]  ${clc.green(message)} +${clc.green(diff.toString())}ms`);
+    this.lastTimestamp = currentTimestamp
   }
 
 
--- src/@nestjs/core/nest-application.ts
+++ src/@nestjs/core/nest-application.ts
@@ -1,5 +1,6 @@
-import express,{Express} from 'express'
+import express,{Express,Request as ExpressRequest,Response as ExpressResponse,NextFunction} from 'express'
 import { Logger } from "./logger";
+import path from 'path'
 export class NestApplication {
   // 内部私有化一个express实例
   private readonly expressApp: Express = express();
@@ -17,6 +18,26 @@ export class NestApplication {
       const prefix = Reflect.getMetadata('prefix', Controller) || '/';
       // 开始解析路由
       Logger.log(`${Controller.name} {${prefix}}`, 'RoutesResolver');
+ 
+      const controllPrototype = Reflect.getPrototypeOf(controller);
+      // 遍历类原型上的方法名
+      for (const methodName of Object.getOwnPropertyNames(controllPrototype)) {
+        // 获取原型上的方法，index
+        const method = controllPrototype[methodName];
+        // 获取方法上的元数据(方法名，请求方法，请求路径)
+        const httpMethod = Reflect.getMetadata('method', method);
+        const pathMetadata = Reflect.getMetadata('path', method);
+
+        if (!httpMethod) continue; // 如果没有方法名，跳过
+        const routePath = path.posix.join('/',prefix,pathMetadata); // 使用内置的path模块处理，可以超级智能的处理路径
+        // 配置路由，当客户端以httpMethod请求routePath时，执行对应路径的函数进行处理
+        this.expressApp[httpMethod.toLowerCase()](routePath,(res:ExpressResponse,req:ExpressRequest,next:NextFunction)=>{
+          const result = method.call(controller);
+          res.send(result)
+        })
+        Logger.log(`Mapped {${routePath}, ${httpMethod}} route`, 'RoutesResolver');
+      }
+      Logger.log(`AppModule dependencies initialized`, 'InstanceLoader');
     }
   }
   // 启动http服务器
